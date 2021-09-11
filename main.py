c73b48c47ce62764ccb4f2d6ea64f40383d8443e
YulyaSarana <fosse@mail.ru>
2021-09-11 13:41:22 +0300

    creating your first homework

---
+++ main.py
@@ -0,0 +1,62 @@
+# First homework
+# 1. string
+Hi = "Вадим! Привет."
+print(Hi)
+# 2. integer
+year = 2021
+print(year)
+# 3. float
+pi = 3.1415926535
+print(pi)
+# 4. bytes
+a = bytes(5)
+print(a)
+# 5. list
+L = [1, 2, 3]
+print(L[1])
+print(L[0])
+# 6. tuple
+T = tuple((123, 'qa', 4.4, "Yulia"))
+print(T[0])
+print(T[1:4])
+# 7. set
+M = {1, 2, 'QA', 1.5}
+print(M)
+# 8. frozenset
+frozen_set = frozenset([1, 2, 3, 4])
+print(frozen_set)
+# 9. dict
+d = dict({1: 'сахар', 2: 'соль', 3: 'сода'})
+print(d[2])
+# 10.
+print(type(Hi))
+print(type(year))
+print(type(pi))
+print(type(a))
+print(type(L))
+print(type(T))
+print(type(M))
+print(type(frozen_set))
+print(type(d))
+# 11.
+animal = 'кот'
+name = ' Вася'
+animal_name = animal + name
+print(animal_name)
+# 12.
+x1 = 30
+x2 = 20
+y = x1 + x2
+print(y)
+# 13.
+y1 = x1 / x2
+print(y1)
+# 14.
+y2 = x1 * x2
+print(y2)
+# 15.
+y3 = x1 // x2
+print(y3)
+# 16.
+y4 = x1 % x2
+print(y4)
