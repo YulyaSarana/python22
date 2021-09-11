f3f003bf25dbb3e054abb075d0df0bf4f8d7bbad
YulyaSarana <fosse@mail.ru>
2021-09-11 18:37:58 +0300

    update: second homework

---
+++ second homework.py
@@ -0,0 +1,42 @@
+# second homework
+# 1.
+animal = 'cat'
+index = '1'
+# 2.
+x1 = 3
+x2 = 15000
+x3 = 50
+x4 = 789
+# 3.
+y1 = 3.1415926535
+y2 = 1.5
+y3 = 255.5
+# 4.
+b_t = True
+b_f = False
+print(x1 >= x2)
+print(x1 <= x2)
+print(x1 != x2)
+print(x2 >= x4)
+print(x4 >= x3)
+print(x2 != x4)
+print(x4 >= x4)
+print(x2 > x1)
+print(x1 >= x3)
+print(x3 < x4)
+print(x3 != x4)
+print(x3 >= x2)
+print(x1 < x4)
+print(x4 < x2)
+print(x1 >= x4)
+# 5.
+print(y1 >= y2)
+print(y1 <= y2)
+print(y1 != y3)
+print(y2 >= y1)
+print(y3 >= y3)
+print(y2 != y3)
+print(y2 >= y1)
+print(y3 > y2)
+print(y1 >= y3)
+# 6.
